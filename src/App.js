d93c3373f26bb827f8b9fb5db2654f48ebb7a10c
daras <daras@kali>
2024-07-13 01:33:57 +0200

    categories boxes added

--- src/App.js
+++ src/App.js
@@ -77,22 +77,17 @@ import { ethers } from 'ethers';
 
 
 
-
-
-
-
-
 function App() {
   return (
     <div className="App">
       <Router >
         <header className="App-header bg-blue-500 text-red p-4">
           <h1 className="text-2xl font-bold text-red">CRYPTOFUND</h1>
-          <p>Welcome to CryptoFund, your decentralized and anonymous way of croudfunding</p>
+          <p>Welcome to CryptoFund, your decentralized and anonymous way of croudfunding!</p>
           <Nav />
-        </header>
+        </header> 
         <main className="p-4">
-          <Routes>
+          <Routes> 
             <Route path="/" element={<Home />} />
           </Routes>
         </main>
