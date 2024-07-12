a8e4fce418fa4323d1322163697a79eb2be6563a
Jonathan Eberle <jonathan.eberle@gmx.de>
2024-07-12 22:50:13 +0200

    fix tailwind postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-}
-=======
-module.exports = {
-	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-	darkMode: false, // or 'media' or 'class'
+	content: [
+	  "./src/**/*.{js,jsx,ts,tsx}",
+	],
 	theme: {
 	  extend: {},
 	},
-	variants: {
-	  extend: {},
-	},
 	plugins: [],
-  }
->>>>>>> 59b1ede2ccdec810678e0dc1ea69243cd5789474
+}
